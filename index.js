6287f013922787bf4a806c66abbbcf664db9c4fc
Jethet <henriette.hettinga@protonmail.com>
2021-07-27 15:36:49 +0200

    Set up project code

---
+++ index.js
@@ -0,0 +1,78 @@
+// ARRAY WITH CHATBOT REPLIES (STRINGS)
+const replies = [
+  "Hello, Welcome to IronChat!",
+  "Ironhack's coding LAB a day keeps the bugs away!",
+  "Thank you for your visit. See you soon!",
+  "Goodbye!",
+  "Nooo way! Same here!!!",
+  "How does that feel on a scale from 1 to 10 ?",
+  "Hmmmm ... interesting!",
+  "That could be a problem.",
+  "Do not go to the bathroom in a dream. It’s a trap!",
+  "It does make sense.",
+  "Why is that?",
+  "Who?",
+];
+
+// GET THE DOM ELEMENTS
+const chat = document.getElementById("chat");
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("message-send-img");
+
+// HELPER FUNCTIONS
+function sendMessage() {
+  const inputValue = messageInput.value;
+
+  displayMessage(inputValue, "Me");
+  messageInput.value = "";
+}
+
+function chatbotReply() {
+  const randomIndex = Math.floor(Math.random() * replies.length);
+  const randomMessage = replies[randomIndex];
+
+  displayMessage(randomMessage, "Chatbot");
+}
+
+function displayMessage(message, sender) {
+  const tempDiv = document.createElement("div");
+
+  if (sender === "Chatbot") {
+    tempDiv.innerHTML = `
+    <div class="message">
+        <img src="./images/ironhack.png" alt="Avatar photo" class="avatar">
+        <h3 class="headline">${sender}</h3>
+        <p>${message}</p>
+      </div>
+  `;
+  } else if (sender === "Me") {
+    tempDiv.innerHTML = `
+    <div class="message align-right">
+        <img src="./images/ironhack.png" alt="Avatar photo" class="avatar">
+        <h3 class="headline">Me</h3>
+        <p>${message}</p>
+      </div>
+      `;
+  }
+
+  const newMessage = tempDiv.children[0];
+
+  chat.appendChild(newMessage);
+
+  chat.scrollTop = chat.scrollHeight;
+
+  if (sender === "Me") {
+    setTimeout(chatbotReply, 2000);
+  }
+}
+
+// EVENT LISTENERS
+sendButton.addEventListener("click", function () {
+  sendMessage();
+});
+
+messageInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    sendMessage();
+  }
+});
