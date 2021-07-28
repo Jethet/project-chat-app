b6b66190d5ce2f050ec8329e3602e9f2f2796d50
Jethet <henriette.hettinga@protonmail.com>
2021-07-28 16:09:14 +0200

    Add project set-up; first basic version

---
+++ replies.js
@@ -0,0 +1,20 @@
+// Hard-coded replies to be used in the chat app (add your own, if you like)
+
+const replies = [
+  "Hello, Welcome to your personal Chat App!",
+  "Some coding a day keeps the bugs away!",
+  "Thank you for your visit. See you soon!",
+  "Goodbye!",
+  "Nooo way! Same here!!!",
+  "How does that feel on a scale from 1 to 10 ?",
+  "Hmmmm ... interesting!",
+  "That could be a problem.",
+  "Do not go to the bathroom in a dream. It’s a trap!",
+  "It does make sense.",
+  "Why is that?",
+  "Who?",
+  "That's great!",
+  "Whatever you say",
+  "I'd like to hear more about that",
+  "Mm-mm, so what are you saying?"
+];
