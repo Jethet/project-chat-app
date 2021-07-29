9ac9ec0695a234bee059755d6506cae5165adcab
Jethet <henriette.hettinga@protonmail.com>
2021-07-29 11:51:45 +0200

    Add first outstanding issues

---
+++ main.js
@@ -0,0 +1,8 @@
+// create message elements that are added as nodes/child elements
+
+// the class should be message-left if the message is a hard-coded message that
+//   is randomly taken from the 'replies' array
+// the class should be message-right if the message is entered dynamically by the user
+
+// the input area needs to have functionality: message text to be grabbed and added
+//    to the message-right element and button submit function
